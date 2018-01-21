577dce31ed273f579f57b221dd1f485a11727a81
fellipegpbotelho <fellipegpbotelho@gmail.com>
2018-01-21 16:16:37 -0200

    first commit

---
+++ src/Html.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Solid\Html;
+
+class Html
+{
+    public function img($src)
+    {
+        $tag = $this->attributes('<img src="%s">');
+
+        //return '<img src="'.$src.'">';
+
+        return $tag;
+    }
+
+    public function a(string $href, string $anchor)
+    {
+        $tag = $this->attributes('<a href="%s"%s>%s</a>');
+
+        $tag->attr[0] = $href;
+        $tag->attr[1] = $anchor;
+
+        return $tag;
+    }
+
+    private function attributes($mask)
+    {
+        $tag = new class {
+
+            private $attributes;
+
+            public  function attribute(array $attributes)
+            {
+                $result = [];
+
+                foreach($attributes as $key => $value){
+                    $result[] = $key . '="'.$value.'"';
+                }
+
+                $this->attributes = ' '.implode(' ', $result);
+            }
+
+            public function __toString()
+            {
+                return sprintf($mask, $this->attr[0], $this->attributes, $this->attr[1]);
+            }
+        };
+
+        return $tag;
+    }
+}
---
+++ tests/AttributesTests.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Solid\Html;
+
+class AttributesTests extends \PHPUnit_Framework_TestCase
+{
+    public function testCriarAttributosHtmlParaTags()
+    {
+        $attributes = new Attributes([
+            'class' => 'btn btn-default',
+            'data-modal' => '#login',
+            'id' => 'login'
+        ]);
+
+        $this->assertEquals(' class="btn btn-default" data-modal="#login" id="login"', $attributes);
+    }
+}
